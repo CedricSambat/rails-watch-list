e819253633ed40325f67002c9db6ad37ab1acdeb
cedric sambat <cedricsambat9818@gmail.com>
2022-10-07 11:31:01 +0400

    watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,24 @@ class ListsController < ApplicationController
     @list = List.new(params[:bookmark])
     @list = List.save
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
 
 private
